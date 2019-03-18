1de7b7fcac150b44eea9b8d0b772c5e92b983e75
nkroker <nikhil.tyagi@sait.ac.in>
2019-03-18 19:28:35 +0530

    added some style to the edit user page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,15 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#livechange').attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
