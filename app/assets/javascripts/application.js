281490f811ba0578c46010a301d16723deeb6b23
nkroker <nikhil.tyagi@sait.ac.in>
2019-03-19 14:30:06 +0530

    Added styling and hidden trigger on profile pic upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,15 +17,3 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
-
-function readURL(input) {
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-
-		reader.onload = function (e) {
-			$('#livechange').attr('src', e.target.result);
-		}
-
-		reader.readAsDataURL(input.files[0]);
-	}
-}
