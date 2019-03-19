ecc64ade8e1f5149ce50e300a658bd0c4003e2f1
nkroker <nikhil.tyagi@sait.ac.in>
2019-03-19 14:43:21 +0530

    added root route diversion by click on brand name

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_scope :user do
   # 	root "devise/sessions#new"
   # end
-  root 'welcome#index'
+  get 'welcome/index'
+  root to: 'welcome#index'
 
 
 end
