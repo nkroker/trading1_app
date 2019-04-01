8f7f3901bfb13dd51860469595dea81ecec6e66c
nkroker <nikhil.tyagi@sait.ac.in>
2019-04-01 08:19:43 +0530

    Added protect_from_forgery into ChargesController

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,6 @@
 class ChargesController < ApplicationController
+  # skip_before_action :verify_authenticity_token  
+  protect_from_forgery
   Stripe.api_key = "sk_test_hMWYftqrT34GjDz1Jm5B8cbt004O9VxlOA"
 
   def new
@@ -12,6 +14,7 @@ class ChargesController < ApplicationController
         email: current_user.email,
     })
 
+    flash[:notice] = customer
 
     current_user.stripe_customer = customer.id
     current_user.save
