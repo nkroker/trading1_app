ac55e06b16adbe1c04cf88a0f97d3272f63965c7
nkroker <nikhil.tyagi@sait.ac.in>
2019-04-09 13:33:19 +0530

    Fixed rolify isses with role querying

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
   rolify
-  resourcify
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:login]
   
 
-  has_many :investments
+  has_many :investments 
   has_one_attached :image
-  has_one :role
 
   validates :contact_no,{ presence: true, uniqueness: true, length: { is: 10 } }
   validates :password,{ format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])/, message: "Password must contain at least 1 digit, 1 special char, 1 Capital Letter, 1 Small Letter and minimum 8 character"}, presence: true, allow_nil: true}
