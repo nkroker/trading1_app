cc21d977faf0b3806db353f087ae65b8f77112b4
nkroker <nikhil.tyagi@sait.ac.in>
2019-04-11 18:56:34 +0530

    Added admin related customer's fetch

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   # load_and_authorize_resource
   def index
-    @investments = Investment.all
+    @investments = Investment.where(customer_id: (User.where(invited_by: current_user.id).first.id)).first
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,13 @@ class Ability
 
   def initialize(user)
     can :manage, :all if user.has_role? :admin
+    can :manage, :all if user.has_role? :customer
+
+
+    if user.is_customer?
+      can :manage, :all
+    end
+    # can :create, :Investment if user.has_role? :customer
     # cannot [:index], AdminController if user.has_role? :customer
   end
 end
